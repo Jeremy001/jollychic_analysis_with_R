40a458b553d96eaf51dff9c13271e5511070ea0e
jeremy001 <diyiduan@126.com>
2018-01-11 11:59:46 +0800

    jollychic analysis

--- 采购需求与配货结果分析.R
+++ 采购需求与配货结果分析.R
@@ -11,3 +11,20 @@ impala <- src_impala(
   driver = "Cloudera ODBC Driver for Impala",
   dsn = 'jolly_impala_64'
 )
+
+# 2.查询数据
+pur_outing_detail <- tbl(impala, 
+                         sql("SELECT * 
+                              FROM zybiro.neo_pur_demand_peihuo_detail"))
+## 修改order_id和sku_id的类型
+pur_outing_detail$order_id <- as.character(pur_outing_detail$order_id)
+pur_outing_detail$sku_id <- as.character(pur_outing_detail$sku_id)
+
+pur_outing_detail %>% 
+  filter(order_id == 27077099) %>% 
+  View()
+
+pur_outing_detail %>% 
+  summarise(a = avg(send_num))
+
+min(pur_outing_detail$order_id, na.rm = TRUE)
